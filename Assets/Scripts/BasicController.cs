571561e60d1fc2800908cbd4bb48265e0c82bd03
Kasiah Butler <kasiah.butler@sea.aie.edu>
2017-01-04 14:53:16 -0800

    Initial Hotdog

---
+++ Assets/Scripts/BasicController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class BasicController : NetworkBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public bool canMove;
+
+    private Rigidbody thisRB;
+
+    private float horInput = 0;
+    private float verInput = 0;
+
+    private Vector3 playerForce;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 30;
+        jumpForce = 30;
+        canMove = true;
+
+        thisRB = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        horInput = Input.GetAxisRaw("Horizontal");
+        verInput = Input.GetAxisRaw("Vertical");
+
+        if (canMove)
+        {
+            handleInput();
+        }
+    }
+
+    void handleInput()
+    {
+        playerForce = new Vector3(horInput * speed, verInput * jumpForce, 0);
+
+        thisRB.AddForce(playerForce);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody playerRigidbody;
+
+    private float horizontalInput;
+    private Vector3 playerAcceleration;
+    private Vector3 playerVelocity;
+    private Vector3 gravity;
+
+    private float Speed;
+    public float speed { get { return Speed; } set { Speed = value; } }
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerRigidbody = this.GetComponent<Rigidbody>();
+        speed = 10;
+	}
+
+    void FixedUpdate ()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        playerAcceleration = new Vector3(horizontalInput * speed / playerRigidbody.mass, 0, 0);
+        playerAcceleration.y -= 9.8f;
+        playerVelocity = playerAcceleration;
+
+        moveHorizontal();
+    }
+
+    void moveHorizontal()
+    {
+        playerRigidbody.velocity = playerVelocity;
+    }
+}
